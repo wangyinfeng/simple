cfabe21c8991d11c073e4bc0c5cc8970d18b1409
wangyinfeng <wang.batman@gmail.com>
2013-10-17 13:30:53 +0800

    todo

---
+++ fibonacci.c
@@ -0,0 +1,23 @@
+/*====================================================================
+ * Description: Kind of classic program practice?
+ *              print the fibonacci number
+ * DATE: 2013/10/17
+ * Modify:
+ *
+===================================================================*/
+/* include */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Macro definition */
+#define true            1
+#define false           0
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
