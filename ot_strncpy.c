0d3ab233215f389a0b29cc5eebb6042bc4009d29
wangyinfeng <wang.batman@gmail.com>
2013-10-21 16:52:24 +0800

    my strncpy

--- ot_strncpy.c
+++ ot_strncpy.c
@@ -1,17 +1,17 @@
-/* my implement of strcpy
+/* my implement of strncpy
  * Test case:
  * 1. Null destination, Null source
- * 2. source is longer than destination
- * 3. source is the destination
+ * 2. n is longer than the destination size
+ * 3. n is ZERO
  *
  * clib behavior:
  * 1. NULL destination/source, segmentation fault
- * 2. source is longer than destination, overlap
- * 3. no error
+ * 2. n is longer than destination, not check; but will stop
+ *    when source is to NULL
+ * 3. nothing copy
  *
  * Conclusion:
- * quite similar to the clib implement. 
- * try to implement the stcncpy()
+ *  quite similar to the clib implementation. 
  * */
 #include <stddef.h>
 #include <stdio.h>
@@ -20,43 +20,62 @@
 char des[8];
 char input[64];
 char *
-ot_strcpy(char *des, const char *src)
+ot_strncpy(char *des, const char *src, size_t n)
 {
     int i=0;
     if (des == NULL) {
-        printf("destination address is invalid\n");
+        printf("Segmentation fault: destination address is invalid\n");
         return des;
     }
     if (src == NULL){
-        printf("source address is NULL, nothing to copy\n");
+        printf("Segmentation fault: source address is NULL, nothing to copy\n");
         return des;
     }
-    /* why use the variable i? I think you are not familiar with
-     * the ++/-- operations
-     * */
-    /*
-    do {
-        *(des+i) = *(src+i);
+    /* there is no way to protect the n>destination size? cannot get the destination */
+
+    while(i<n && ((*des++) = *(src++)))
+    {
         i++;
-    } while(*(src+i) != NULL);
-    */
-    do {
-        *(des++) = *(src++);
-    } while(*src != NULL);
+    }
+//    while(*(des++) = *(src++), (++i < n)); /* if n is ZERO..., this approach at least copy one char */
     printf("%d character copyed\n", i);
     return des;
 }
 
 int 
-/* Seems you never use the main parameters before 
- * Use the command line parameter to do test 
- */
 main(int argc, char **argv)
 {
-	printf("\ninput the string to be copyed: ");
-	fgets(input, 64, stdin);
-	ot_strcpy(des, input);
-	printf("string copyed: %s\n", des);
+//	printf("\ninput the string and the length to be copyed: ");
+//	fgets(input, 64, stdin);
+
+    int len = 0;
+    char *src = NULL;
+    if (argc > 1) {
+        len = atoi(argv[2]);
+        src = argv[1];
+    }
+    else {
+        printf("Please input the string and length, divide by the space\n");
+        return 0;
+    }
+
+	ot_strncpy(des, src, len);
+	printf("string copyed: %s, length : %d\n", des, len);
+
+    return 0;
 }
 
 /* implement in std clib */
+/* will stop when source reach NULL? */
+char *
+_strncpy(to, from, len)
+	char *to;
+	const char *from;
+	int len;
+{
+	char *save = to;
+
+	for (; len-- && (*to = *from); ++from, ++to);
+	return(save);
+}
+
