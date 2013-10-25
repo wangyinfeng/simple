2041ae9f3fccaf46e2da592c2afe50ed93cc61c7
wangyinfeng <wang.batman@gmail.com>
2013-10-25 15:36:21 +0800

    todo

---
+++ big_little_endian.c
@@ -0,0 +1,23 @@
+/*====================================================================
+ * Description: Approach to check the current machine is big endian or
+ 		little endian
+ * DATE: 2013/10/25
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* include */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Macro definition */
+#define true            1
+#define false           0
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
