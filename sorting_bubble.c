2ee60422a80c59856bcff02ca469a712ad0899d7
wangyinfeng <wang.batman@gmail.com>
2013-10-28 17:40:37 +0800

    new task

---
+++ sorting_bubble.c
@@ -0,0 +1,23 @@
+/*====================================================================
+ * Description: Seems you know nothing about sorting, basic algorithm,
+ 		worth to do some practice
+ * DATE: 2013/10/28
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* include */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Macro definition */
+#define true            1
+#define false           0
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
