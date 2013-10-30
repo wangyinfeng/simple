0cf5a1a0d4a36efdec8daa0f07aefa331815bced
wangyinfeng <wang.batman@gmail.com>
2013-10-30 18:49:18 +0800

    queue

---
+++ uucp_queue.c
@@ -0,0 +1,23 @@
+/*====================================================================
+ * Description: Implement the queue, according to the function 
+ *              declaration in the book
+ * DATE: 2013/10/30
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* include */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Macro definition */
+#define true            1
+#define false           0
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
