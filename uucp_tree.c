8912a81513fcfb2b15f37c0a1023d77c3735511a
wangyinfeng <wang.batman@gmail.com>
2013-11-01 10:37:23 +0800

    todo tree

---
+++ uucp_tree.c
@@ -0,0 +1,24 @@
+/*====================================================================
+ * Description: Implement the tree... strange to me
+ *              Also kind of linked list? which left and right two
+ *              arms?
+ * DATE: 2013/11/1
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* include */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Macro definition */
+#define true            1
+#define false           0
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
