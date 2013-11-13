03778bf060a19d504cbfd1036e66e0f174751eda
wangyinfeng <wang.batman@gmail.com>
2013-11-13 15:40:24 +0800

    todo

--- uucp_tree.c
+++ uucp_tree.c
@@ -1,6 +1,6 @@
 /*====================================================================
- * Description: Implement the tree... strange to me
- *              Also kind of linked list? which left and right two
+ * Description: Implement the tree, binary tree... strange to me
+ *              Also kind of linked list? with left and right two
  *              arms?
  * DATE: 2013/11/1
  * Modify:
@@ -15,6 +15,24 @@
 #define true            1
 #define false           0
 
+typedef struct _tree{
+    void *data;
+    struct _tree *left;
+    struct _tree *right;
+}Tree;
+
+/* insert node to the tree, if the data is larger than the parent's
+ * insert to the right leaf, if the data is smaller than the 
+ * parent's, insert to the left leaf, if is euqal to the parent's
+ * insert to the left leaf also. */
+void
+insert(Tree **root, void *data)
+{
+}
+
+void displayTree(Tree *t)
+{
+}
 int
 main(int argc, char **argv)
 {
