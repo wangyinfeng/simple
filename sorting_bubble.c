5ba23d42705a19c2d1b0197dfd47702219a11318
wangyinfeng <wang.batman@gmail.com>
2013-11-27 16:57:27 +0800

    initial done

--- sorting_bubble.c
+++ sorting_bubble.c
@@ -1,8 +1,17 @@
 /*====================================================================
  * Description: Seems you know nothing about sorting, basic algorithm,
- 		worth to do some practice
- * DATE: 2013/10/28
+ *              worth to do some practice
+ *              Bubble sorting, the simplest sorting.
+ *              1. Compare the adjacent elements, move the bigger to the tail;
+ *              2. The remain un-sorting number --;
+ *              3. One traversal decide one biggest element;
+ *              4. Until all element be sorted.
+ *
+ * DATE: 2013/11/27
  * Modify:
+ * TODO:
+ *        Input random number from file, and output the number to console.
+ *        Should have more than one approach.
  * Conclusion:
 ===================================================================*/
 /* include */
@@ -14,9 +23,49 @@
 #define true            1
 #define false           0
 
+int num[] = {2,4,12,34,3,56,23,67,90,1,66,43};
+
+void
+print_num(int n[], int size)
+{
+    int i;
+    for (i=0; i<size; i++){
+        printf("%02d ", n[i]);
+    }
+    printf("\n");
+}
+
+void
+sort_A(int n[], int size)
+{
+    int i = 0;
+    int tmp;
+    do {
+        for (i=0; i<size-1; i++) {
+            if (n[i] > n[i+1]) {
+                tmp = n[i];
+                n[i] = n[i+1];
+                n[i+1] = tmp;
+            }
+        }
+        size--;
+    } while(size);
+}
+
 int
 main(int argc, char **argv)
 {
+    int size;
+    size = sizeof(num)/sizeof(int);
+    if (size < 2)
+        printf("Element number less than 2, quit\n");
+
+    printf("Original sequence:\n");
+    print_num(num, size);
+
+    sort_A(num, size);
+    printf("Sorted sequence:\n");
+    print_num(num, size);
 
     return 0;
 }
