96d1fa680ec630bd4e6dce5bc90b37071264a911
wangyinfeng <wang.batman@gmail.com>
2014-04-24 13:22:51 +0800

    todo: add code to verify the quiz about integers in c

---
+++ integer_quiz.c
@@ -0,0 +1,26 @@
+/*====================================================================
+ * Description: Practice the quiz about integer, know some detail about
+ * integer overflow.
+ * The link for the practice is http://blog.regehr.org/archives/721,
+ * worth to do the test.
+ * DATE: 2014/04/24
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* include */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Macro definition */
+#define false           0
+#define true            1
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
