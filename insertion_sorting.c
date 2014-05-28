d186a023b984f436ef7313d83c2ef9e46e59e9c2
WangYinfeng <wang.batman@gmail.com>
2014-05-28 16:54:46 +0800

    TODO: insertion sorting

---
+++ insertion_sorting.c
@@ -0,0 +1,23 @@
+/*====================================================================
+ * Description: 
+ * DATE: 2014/04/23
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* include */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Macro definition */
+#define false           0
+#define true            1
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
