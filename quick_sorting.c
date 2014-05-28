d7e85935fb061922d4bb8f631e118e710b212142
WangYinfeng <wang.batman@gmail.com>
2014-05-28 16:51:22 +0800

    TODO: implement the quick sorting

---
+++ quick_sorting.c
@@ -0,0 +1,23 @@
+/*====================================================================
+ * Description: Practice the quick sorting
+ * DATE: 2014/05/28
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* include */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Macro definition */
+#define false           0
+#define true            1
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
