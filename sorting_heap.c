530010d2aef874e8c2683aee5d00ae4f9ced5602
WangYinfeng <wang.batman@gmail.com>
2014-06-04 19:19:53 +0800

    TODO: heap sorting

---
+++ sorting_heap.c
@@ -0,0 +1,23 @@
+/*====================================================================
+ * Description: Practice the heap sorting
+ * DATE: 2014/06/04
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* include */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Macro definition */
+#define false           0
+#define true            1
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
