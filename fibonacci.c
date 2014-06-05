932e35fbee8efaf7776f9e857bd4d486e19a8c15
WangYinfeng <wang.batman@gmail.com>
2014-06-05 18:11:29 +0800

    Too slow, is there any faster way

--- fibonacci.c
+++ fibonacci.c
@@ -2,7 +2,9 @@
  * Description: Kind of classic program practice?
  *              print the fibonacci number
  * DATE: 2013/10/17
- * Modify:
+ * Modify: 2014/06/05 implement it.
+ * Fn = Fn-1 + Fn-2 , n=1,2,3... are 1,1,2,3,5...
+ * F0 = 0, F1=F2=1
  *
 ===================================================================*/
 /* include */
@@ -13,11 +15,43 @@
 /* Macro definition */
 #define true            1
 #define false           0
+unsigned int Fn_1, Fn_2;
 
+unsigned int
+fibonacci(unsigned int n)
+{
+    unsigned int x;
+    if (n > 2) {
+        x = fibonacci(n-1) + fibonacci(n-2);
+        return x;
+    }
+    else if (n == 2) {
+        return 1;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else {
+        printf("Invalid 'n'!\n");
+    }
+    return 0;
+}
+
+/* input the n, print the Fn,Fn-1,Fn-2.... */
 int
 main(int argc, char **argv)
 {
-
+    unsigned int n;
+    
+    if (argc < 2) {
+        printf("Please input the 'n'\n");
+        exit(1);
+    }
+    n = atoi(argv[1]);
+    for (; n>0; n--) {
+        printf("%d ", fibonacci(n));
+    }
+    printf("\n");
     return 0;
 }
 
