6f7c406665081463d4002d68dcda82b48ace6991
WangYinfeng <wang.batman@gmail.com>
2014-06-11 15:21:00 +0800

    done. all about linked list

--- josephus_problem.c
+++ josephus_problem.c
@@ -5,6 +5,8 @@
  * DATE: 2014/06/10
  * Modify:
  * Conclusion:
+ * Sad to say still not understand the linked list, and not familiar
+ * with the pointer operation.
 ===================================================================*/
 /* include */
 #include <stdio.h>
@@ -63,7 +65,7 @@ print_josephus(void)
         fprintf(stderr, "empty list\n");
         exit(1);
     }
-    
+
     do {
         printf("[%d]->", cur->id);
         cur = cur->next;
@@ -71,10 +73,9 @@ print_josephus(void)
             printf("Not cycle linked list?\n");
             exit(1);
         }
-            
     } while (cur != poor_men.header);
-    printf("[[%d]]", cur->id);
-    printf("\n");
+
+    printf("[[%d]]\n", cur->id);
 }
 /* linked list delete */
 void
@@ -82,16 +83,21 @@ execute_the_one(unsigned int m)
 {
     NODE *previous;
     NODE *the_one;
-    unsigned int j = 0;
+    unsigned int j = 1;
+    poor_men.current = poor_men.header;
     do {
         previous = poor_men.current;
         poor_men.current = poor_men.current->next;
         j++;
     } while(j < m);
-    the_one = previous->next;
-//    free(the_one);
-    previous->next = poor_men.current->next;
+    poor_men.count--;
     printf("Execute [%d] ", poor_men.current->id);
+    previous->next = poor_men.current->next;
+    poor_men.header = poor_men.current->next;
+
+    the_one = poor_men.current;
+    free(the_one);
+
     printf("The remains are:");
     print_josephus();
 }
@@ -99,13 +105,15 @@ execute_the_one(unsigned int m)
 unsigned int 
 josephus(unsigned int n, unsigned m)
 {
-    unsigned int i;
+    unsigned int i, survior;
     init_josephus(n);
     print_josephus();
-    for (i=n; i>0; i--) {
+    for (i=n; i>1; i--) { /* leave the last one */
         execute_the_one(m);
     }
-    return poor_men.current->id;
+    survior = poor_men.header->id;
+    free(poor_men.header);
+    return survior;
 }
 
 /* input the n and m from command line, where n>1 */
@@ -120,10 +128,10 @@ main(int argc, char **argv)
     n = atoi(argv[1]);
     m = atoi(argv[2]);
     if ( (n<2) || (m<1) ) {
-        fprintf(stderr, "invalid parameters\n");
+        fprintf(stderr, "invalid parameters, n=%d m=%d\n", n,m);
         exit(1);
-     }
-     printf("[%d] mem stand in cycle, the [%d]th man will be exectued\n", 
+    }
+    printf("[%d] mem stand in cycle, the [%d]th man will be exectued\n", 
          n, m);
     p = josephus(n, m);
     printf("stand at position [%d] will survior\n", p);
