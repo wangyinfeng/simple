ccfe434c1a50cc429357b6acf87afef9e3ab4970
Wang Yinfeng <wang.batman@gmail.com>
2015-01-07 11:44:49 -0500

    new task

---
+++ find_the_most.c
@@ -0,0 +1,23 @@
+/*====================================================================
+ * Description: Give a string, such as "1abb55cciijjj", find the character which appears most time.
+ * DATE: 2015/01/07
+ * Modify:
+ * Conclusion:
+===================================================================*/
+/* include */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Macro definition */
+#define false           0
+#define true            1
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
+
