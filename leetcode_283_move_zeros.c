f7e86ebbf5046673ec931294573f0d56fd1f7dac
w <w@u64.com>
2016-03-26 05:06:31 +0800

    a better way

--- leetcode_283_move_zeros.c
+++ leetcode_283_move_zeros.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/*
 void moveZeroes(int* nums, int numsSize) {
     int i, j, n = 0;
     if (nums == NULL || numsSize <0)
@@ -20,12 +21,32 @@ void moveZeroes(int* nums, int numsSize) {
         }
     }
 }
+*/
 
+/*This way is much more decent, clear and easy to understand
+ * performance 4 times better than the above */
+void moveZeroes(int* nums, int numsSize) {
+    int i=0, j=0;
+    while (i<numsSize) {
+        if (nums[i]!=0){
+            nums[j]=nums[i];
+            j++;
+            i++;
+        } 
+        else {
+            i++;
+        }
+    }
+    while (j<numsSize){
+        nums[j]=0;
+        j++;
+    }
+}
 int main(int argc, char **argv)
 {
     int i;
-    //int nums[] = {0,0,1,2,0,0,4,0,5};
-    int nums[] = {1, 0,0,0,0,0};
+    int nums[] = {0,0,1,2,0,0,4,0,5};
+    //int nums[] = {1, 0,0,0,0,0};
     int numsSize = sizeof(nums)/sizeof(int);
     moveZeroes(nums, numsSize);
     for (i=0; i<numsSize; i++)
