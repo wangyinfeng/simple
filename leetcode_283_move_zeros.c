94ef5e106c6362bbda83c05a0c28e075ec92c5e9
w <w@u64.com>
2016-03-26 00:09:42 +0800

    need to improve performance

---
+++ leetcode_283_move_zeros.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void moveZeroes(int* nums, int numsSize) {
+    int i, j, n = 0;
+    if (nums == NULL || numsSize <0)
+        return;
+    // a little silly here
+    for (i=0; i<numsSize; i++) {
+        if (nums[i] == 0)
+            n++;
+    }
+
+    for (i=0; i<numsSize; i++) {
+        if (i > 0 && nums[i-1] == 0 && n-->0)
+            i--;
+        if (nums[i] == 0) {
+            for (j=i; j<numsSize-1; j++)
+                nums[j] = nums[j+1];
+            nums[numsSize-1] = 0;
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+    //int nums[] = {0,0,1,2,0,0,4,0,5};
+    int nums[] = {1, 0,0,0,0,0};
+    int numsSize = sizeof(nums)/sizeof(int);
+    moveZeroes(nums, numsSize);
+    for (i=0; i<numsSize; i++)
+        printf("%d ", nums[i]);
+    printf("\n");
+
+    return 0;
+}
+
