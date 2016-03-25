04a5de9c1cf19b4737c4b35b295b8d0a26731dbf
w <w@u64.com>
2016-03-25 21:00:14 +0800

    copied

--- leetcode_136_single_number.c
+++ leetcode_136_single_number.c
@@ -3,29 +3,26 @@
 #include <stdio.h>
 
 int singleNumber(int* nums, int numsSize) {
-    if (nums == NULL || numsSize<=2)
+    if (nums == NULL)
         return -1;
-    int i, j;
-    for (i=0; i<numsSize; i++) {
-        for (j=i+1; j<numsSize; j++) {
-            if (nums[i] == nums[j])
-                continue;
-            else if (j == numsSize -1)
-                return i;
-        }
+    int result = 0;
+    int i;
+    for (i=0; i<numsSize; i++){
+        /* It's bloody trick to use the XOR to determine the same number
+         * of number 2. XOR not work for odd numbers.
+         * Another way is to sort the array then count the same number */
+        result ^= nums[i];
     }
-    return -2;
+    return result;
 }
 
 int main(int argc, char **argv)
 {
-    int nums[] = {1, 3, 1, 5, 0, 5, 0};
+    int nums[] = {1, 3, 1, 5, 3, 5, 0};
+    //int nums[] = {1};
     int numsSize = sizeof(nums)/sizeof(int);
     int num = singleNumber(nums, numsSize);
-    if (num >=0 && num < numsSize)
-        printf("The number %d in position %d is single.\n", nums[num], num);
-    else
-        printf("Invalid!\n");
+    printf("The number %d is single.\n", num);
     return 0;
 }
 
